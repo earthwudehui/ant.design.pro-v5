0244067d463162d79d012bd596242bc734a8c954
earthwudehui <xkwdh@126.com>
2020-10-21 10:36:41 +0800

    初始化项目

--- src/app.tsx
+++ src/app.tsx
@@ -42,13 +42,8 @@ export const layout = ({
 }: {
   initialState: { settings?: LayoutSettings; currentUser?: API.CurrentUser };
 }): BasicLayoutProps => {
-  // add:使用lodash 判断路由是否空
-  let routes = {routes:[]};
-  if(initialState?.currentUser&&initialState?.currentUser.menudata){
-    routes = initialState?.currentUser.menudata
-  }
   return {
-    menuDataRender:()=>routes,
+    menuDataRender:()=>(initialState?.currentUser&&initialState?.currentUser.menudata)?(initialState?.currentUser.menudata):{routes:[]},
     rightContentRender: () => <RightContent />,
     disableContentMargin: false,
     footerRender: () => <Footer />,
